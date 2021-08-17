0cee29649f09b813e94075bf987ebf3009f8bc16
Arjunpatidar9754 <67516896+Arjunpatidar9754@users.noreply.github.com>
2021-08-18 01:15:44 +0530

    LAMPlearning

---
+++ src/components/body/home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        <div className="home_page">
+            <h2>Hello everyone!</h2>
+            <h3>--Welcome to LAMPlearning--</h3>
+            <p>
+                This is a best E-learning platform.
+                you can code here and practice more on your skills, you can read the documentation,
+                you will get an access to download pdf of Tutorials and read it offline in just a simple login, you can consider pdf as notes,
+                we trying to provides you a best discussion forum to clear your doubts.
+            </p>
+            <h6>It's Amazing, hurreee!! you will get everything in a single platform.</h6>
+        </div>
+    )
+}
+
+export default Home
